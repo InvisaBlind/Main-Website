04beaaa735cd5bc01d0b684a2cae59c20b699453
Henry Fitzgerald <henryfitzgerald@icloud.com>
2025-12-05 02:20:48 -0500

    remove webFinalProject

--- webFinalProject/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myapp.models import course
-
-# Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'skill_rating', 'category')
-
-admin.site.register(course, CourseAdmin)
--- webFinalProject/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("course/", views.courses, name="courses"),
-    path("about/", views.about, name="about"),
-    path("lesson/", views.lesson, name="lesson"),
-]
